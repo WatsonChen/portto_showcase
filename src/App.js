de01bf3b619fd09a708338bc8c89c5185d618e75
watson chen <chenpkpk@gmail.com>
2021-12-31 17:10:57 +0800

    import Navigate for redirect of react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { List, Detail } from "pages";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="*" element={<Navigate to="/list" />} />
         <Route path="/list" element={<List />} />
         <Route path="/detail" element={<Detail />} />
       </Routes>
