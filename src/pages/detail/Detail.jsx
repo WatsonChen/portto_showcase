2ee3c8ef1bb2729bab51e8ad92e832e4720cd2cf
watson chen <chenpkpk@gmail.com>
2021-12-31 23:52:43 +0800

    updated Detail component

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { Row, Col, Divider, Card, Typography, Layout } from "antd";
+import React, { useEffect, useState } from "react";
+import { Row, Col, Button, Card, Typography, Layout } from "antd";
 import styles from "./Detail.module.scss";
 import { LeftOutlined } from "@ant-design/icons";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { apiGetDetail } from "api/api";
 
 export const Detail = () => {
   // === antd ===
@@ -11,6 +12,18 @@ export const Detail = () => {
   const { Header, Footer, Content } = Layout;
   // === react route dom ===
   const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const address = searchParams.get("address");
+  const id = searchParams.get("id");
+
+  // useState
+  const [tableData, setTableData] = useState([]);
+  useEffect(() => {
+    apiGetDetail(address, id).then((res) => {
+      setTableData(res.data.assets);
+    });
+  }, []);
+
   return (
     <>
       <Layout>
@@ -32,21 +45,42 @@ export const Detail = () => {
         <Content className="p-10">
           <Row gutter={24}>
             <Col span={24}>
-              <Card
-                hoverable
-                cover={
-                  <img
-                    alt="example"
-                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                  />
-                }
-              >
-                <Meta title="Europe Street beat" description="description" />
-              </Card>
+              {tableData && (
+                <>
+                  <Card
+                    hoverable
+                    cover={
+                      <img
+                        alt={`${tableData.name}`}
+                        src={`${tableData.image_url}`}
+                      />
+                    }
+                  >
+                    <Meta
+                      title={`${tableData.name}`}
+                      description={`${tableData.description}`}
+                    />
+                  </Card>
+                  <div className="text-center">
+                    <Button
+                      onClick={tableData.permalink}
+                      type="primary"
+                      style={{
+                        position: "fixed",
+                        bottom: "3rem",
+                        "font-size": "1.5rem",
+                        padding: "1rem",
+                        height: "4rem",
+                      }}
+                    >
+                      Permalink
+                    </Button>
+                  </div>
+                </>
+              )}
             </Col>
           </Row>
         </Content>
-        <Footer className="text-center">Portto case</Footer>
       </Layout>
     </>
   );
