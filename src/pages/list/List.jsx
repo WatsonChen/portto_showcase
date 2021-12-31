c14421bf282524876cced135397c83f7852b0d56
watson chen <chenpkpk@gmail.com>
2021-12-31 23:53:04 +0800

    add infinite scroll function

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Row, Col, Divider, Card, Typography, Layout } from "antd";
 import styles from "./List.module.scss";
 import { apiGetList } from "api/api";
 import { useNavigate } from "react-router-dom";
+import { LoadingOutlined } from "@ant-design/icons";
 
 export const List = () => {
   // === antd ===
@@ -10,29 +11,39 @@ export const List = () => {
   const { Meta } = Card;
   const { Header, Footer, Content } = Layout;
   const [tableData, setTableData] = useState([]);
+  // useState
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  // useRef
+  const ref = useRef();
   // === react router dom ===
   const nevigate = useNavigate();
   // === useState ===
-  // const [offset, setOffset] = useState(0);
-  // const [limit, setLimit] = useState(5);
-  // const [callApi, setCallApi] = useState(false)
-  const handleScroll = (e) => {
-    const target = e.target;
-    console.log("1", target.scrollHeight);
-    console.log("2", target.scrollTop);
-    console.log("3", target.clientHeight);
-    if (target.scrollHeight - target.scrollTop == target.clientHeight) {
-      console.log("!!!!!");
-    }
-  };
+  const [offset, setOffset] = useState(0);
+  const [isBottom, setIsBottom] = useState(false);
+
+  let observer = new IntersectionObserver(([entry]) => {
+    if (entry) setIsIntersecting(entry.isIntersecting);
+  });
   useEffect(() => {
-    apiGetList(0, 5).then((res) => {
-      setTableData(res.data.assets);
-    });
+    observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
   }, []);
-  // useEffect(() => {
-  //   console.log("At The Bottom"); //Add in what you want here
-  // }, [bottom]);
+
+  useEffect(() => {
+    if (isIntersecting && !isBottom) {
+      apiGetList(offset, 5).then((res) => {
+        setTableData(tableData.concat(res.data.assets));
+        if (res.data.assets.length == 5) {
+          setOffset(offset + 5);
+        }
+        if (res.data.assets.length < 5) {
+          setIsBottom(true);
+        }
+      });
+    }
+  }, [isIntersecting]);
 
   return (
     <>
@@ -42,20 +53,12 @@ export const List = () => {
             <Title level={2}>List</Title>
           </Divider>
         </Header>
-        <Content
-          className="p-10"
-          onScroll={handleScroll}
-          style={{ height: "900px" }}
-        >
+        <Content className="p-10" style={{ height: "100%" }}>
           <Row gutter={24}>
             {tableData &&
               tableData.map((item) => {
                 return (
-                  <Col
-                    span={12}
-                    className={styles.cardCol}
-                    onScroll={handleScroll}
-                  >
+                  <Col span={12} className={styles.cardCol}>
                     <Card
                       onClick={() => {
                         nevigate(
@@ -67,12 +70,19 @@ export const List = () => {
                         <img alt={`${item.name}`} src={`${item.image_url}`} />
                       }
                     >
-                      <Meta title="Europe Street beat" />
+                      <Meta title={`${item.name}`} />
                     </Card>
                   </Col>
                 );
               })}
           </Row>
+          <div ref={ref} id="test">
+            {isBottom ? (
+              "已經沒有更多資料了"
+            ) : (
+              <LoadingOutlined style={{ "font-size": "2rem" }} />
+            )}
+          </div>
         </Content>
         <Footer className="text-center">Portto case</Footer>
       </Layout>
